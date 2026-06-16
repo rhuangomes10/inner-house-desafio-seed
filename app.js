e5edf2990f49a60832deea2cc0af0244404b529d
rhuangomes10 <rhuancraft189gg@gmail.com>
2026-06-16 20:56:36 -0300

    feat: adicionando rotas

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT;
+const path = require('path')
 
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('Olá, mundo! Projeto Node.js rodando.');
+  res.sendFile(path.join(__dirname, "/public/home/index.html"));
 });
+
+app.get('/blog', (req,res) =>{
+    res.sendFile(path.join(__dirname, "/public/blog/blog.html"))
+})
+app.get('/solucao', (req,res) =>{
+    res.sendFile(path.join(__dirname, "/public/solution/solution.html"))
+})
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
