8346a0a0826e76198b604f9a8b5bc334208687e9
rhuangomes10 <rhuancraft189gg@gmail.com>
2026-06-16 21:09:06 -0300

    refactor: rotas mais definidas

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
 const port = process.env.PORT;
-const path = require('path')
+const path = require("path");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/home/index.html"));
+app.use("/", require("./api/routes/index"));
+app.use("/blog", require("./api/routes/blog"));
+app.use("/solucao", require("./api/routes/solucao"));
+
+app.use((req, res) => {
+  res.status(404).send("Página não encontrada");
 });
-
-app.get('/blog', (req,res) =>{
-    res.sendFile(path.join(__dirname, "/public/blog/blog.html"))
-})
-app.get('/solucao', (req,res) =>{
-    res.sendFile(path.join(__dirname, "/public/solution/solution.html"))
-})
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
